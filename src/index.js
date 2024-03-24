d285149a3652d0c56d42d763b7bb72833acbf7d7
Rayan Obeid <rayan.obeid93@gmail.com>
2024-03-24 23:36:38 +0200

    edit

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { BrowserRouter } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename='/Task6_2'>
+    <BrowserRouter basename='/Task6_2/'>
     <App />
     </BrowserRouter>
   </React.StrictMode>
